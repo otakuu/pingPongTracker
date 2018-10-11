614df25878435e14a4e4afda1f9579b1748aa6a0
otakuu <dominic.allemann@gmail.com>
2018-10-11 09:46:57 +0200

    inital

--- tracker.py
+++ tracker.py
@@ -4,11 +4,12 @@ import numpy as np
 kernel = np.ones((5,5),np.uint8)
 
 # Take input from webcam
-cap = cv2.VideoCapture(-1)
+cap = cv2.VideoCapture(0)
 
 # Reduce the size of video to 320x240 so rpi can process faster
-cap.set(3,320)
-cap.set(4,240)
+#cap.set(3,320)
+#cap.set(4,240)
+
 
 def nothing(x):
     pass
@@ -21,92 +22,72 @@ cv2.namedWindow('tracking')
 
 
 # Creating track bar for min and max for hue, saturation and value
-# You can adjust the defaults as you like
-cv2.createTrackbar('hmin', 'HueComp',12,179,nothing)
-cv2.createTrackbar('hmax', 'HueComp',37,179,nothing)
+cv2.createTrackbar('hmin', 'HueComp',95,179,nothing)
+cv2.createTrackbar('hmax', 'HueComp',115,179,nothing)
 
-cv2.createTrackbar('smin', 'SatComp',96,255,nothing)
-cv2.createTrackbar('smax', 'SatComp',255,255,nothing)
+cv2.createTrackbar('smin', 'SatComp',17,255,nothing)
+cv2.createTrackbar('smax', 'SatComp',139,255,nothing)
 
-cv2.createTrackbar('vmin', 'ValComp',186,255,nothing)
+cv2.createTrackbar('vmin', 'ValComp',152,255,nothing)
 cv2.createTrackbar('vmax', 'ValComp',255,255,nothing)
-
-# My experimental values
-# hmn = 12
-# hmx = 37
-# smn = 145
-# smx = 255
-# vmn = 186
-# vmx = 255
 
 
 while(1):
 
-    buzz = 0
-    _, frame = cap.read()
-
-    #converting to HSV
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    hue,sat,val = cv2.split(hsv)
-
-    # get info from track bar and appy to result
-    hmn = cv2.getTrackbarPos('hmin','HueComp')
-    hmx = cv2.getTrackbarPos('hmax','HueComp')
+    is_sucessfully_read, frame = cap.read()
     
+    if(is_sucessfully_read):
+        
+        #converting to HSV
+        cv2.flip(frame,1)
+        hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        hue,sat,val = cv2.split(hsv)     
 
-    smn = cv2.getTrackbarPos('smin','SatComp')
-    smx = cv2.getTrackbarPos('smax','SatComp')
+        # get info from track bar and appy to result
+        hmn = cv2.getTrackbarPos('hmin','HueComp')
+        hmx = cv2.getTrackbarPos('hmax','HueComp')
 
+        smn = cv2.getTrackbarPos('smin','SatComp')
+        smx = cv2.getTrackbarPos('smax','SatComp')
 
-    vmn = cv2.getTrackbarPos('vmin','ValComp')
-    vmx = cv2.getTrackbarPos('vmax','ValComp')
+        vmn = cv2.getTrackbarPos('vmin','ValComp')
+        vmx = cv2.getTrackbarPos('vmax','ValComp')
 
-    # Apply thresholding
-    hthresh = cv2.inRange(np.array(hue),np.array(hmn),np.array(hmx))
-    sthresh = cv2.inRange(np.array(sat),np.array(smn),np.array(smx))
-    vthresh = cv2.inRange(np.array(val),np.array(vmn),np.array(vmx))
+        # Apply thresholding
+        hthresh = cv2.inRange(np.array(hue),np.array(hmn),np.array(hmx))
+        sthresh = cv2.inRange(np.array(sat),np.array(smn),np.array(smx))
+        vthresh = cv2.inRange(np.array(val),np.array(vmn),np.array(vmx))
 
-    # AND h s and v
-    tracking = cv2.bitwise_and(hthresh,cv2.bitwise_and(sthresh,vthresh))
+        # AND h s and v
+        tracking = cv2.bitwise_and(hthresh,cv2.bitwise_and(sthresh,vthresh))
 
-    # Some morpholigical filtering
-    dilation = cv2.dilate(tracking,kernel,iterations = 1)
-    closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
-    closing = cv2.GaussianBlur(closing,(5,5),0)
+        # Some morpholigical filtering
+        dilation = cv2.dilate(tracking,kernel,iterations = 1)
+        closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
+        closing = cv2.GaussianBlur(closing,(5,5),0)
 
-    # Detect circles using HoughCircles
-    circles = cv2.HoughCircles(closing,cv2.HOUGH_GRADIENT,2,120,param1=120,param2=50,minRadius=10,maxRadius=0)
-    # circles = np.uint16(np.around(circles))
+        # Detect circles using HoughCircles 100/50
+        circles = cv2.HoughCircles(closing,cv2.HOUGH_GRADIENT,2,120,param1=70,param2=40,minRadius=10,maxRadius=20)
 
-    #Draw Circles
-    if circles is not None:
-            for i in circles[0,:]:
-                # If the ball is far, draw it in green
-                if int(round(i[2])) < 30:
-                    cv2.circle(frame,(int(round(i[0])),int(round(i[1]))),int(round(i[2])),(0,255,0),5)
-                    cv2.circle(frame,(int(round(i[0])),int(round(i[1]))),2,(0,255,0),10)
-				# else draw it in red
-                elif int(round(i[2])) > 35:
-                    cv2.circle(frame,(int(round(i[0])),int(round(i[1]))),int(round(i[2])),(0,0,255),5)
-                    cv2.circle(frame,(int(round(i[0])),int(round(i[1]))),2,(0,0,255),10)
-                    buzz = 1
+        #Draw Circles
+        if circles is not None:
+                for i in circles[0,:]:
+                    print('x: '+str(round(i[0])))
+                    print('y: '+str(round(i[1])))
+                    cv2.circle(frame,(int(round(i[0])),int(round(i[1]))),int(round(i[2])),(0,255,0),1)
+                    cv2.circle(frame,(int(round(i[0])),int(round(i[1]))),2,(0,255,0),5)
 
-	#you can use the 'buzz' variable as a trigger to switch some GPIO lines on Rpi :)
-    # print buzz                    
-    # if buzz:
-        # put your GPIO line here
+        
+        #Show the result in frames
+        cv2.imshow('HueComp',hthresh)
+        cv2.imshow('SatComp',sthresh)
+        cv2.imshow('ValComp',vthresh)
+        cv2.imshow('closing',closing)
+        cv2.imshow('tracking',frame)
 
-    
-    #Show the result in frames
-    cv2.imshow('HueComp',hthresh)
-    cv2.imshow('SatComp',sthresh)
-    cv2.imshow('ValComp',vthresh)
-    cv2.imshow('closing',closing)
-    cv2.imshow('tracking',frame)
-
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
-        break
+        k = cv2.waitKey(5) & 0xFF
+        if k == 27:
+            break
 
 cap.release()
 
--- websocket.py
+++ websocket.py
@@ -1,25 +1,70 @@
-import cv2
-import numpy as np
 import asyncio
 import datetime
 import random
 import websockets
+import cv2
+import numpy as np
 
-# C:\Users\dalleman\AppData\Local\Programs\Python\Python36-32\python.exe websocket.py
+kernel = np.ones((5,5),np.uint8)
+
+# Take input from webcam
+cap = cv2.VideoCapture(0)
+print(str(cap.get(3))+' x '+str(cap.get(4)))
+
+hmn = 95
+hmx = 115
+
+smn = 17
+smx = 139
+
+vmn = 152
+vmx = 255
 
 async def time(websocket, path):
-    player = 0
+    print('connected')
     while True:
-        if(player % 2==0):
-            x = random.randint(0, 410)
-        else:
-            x = random.randint(410, 820)
-        y = random.randint(100, 500)
-        await websocket.send(str(x)+';'+str(y))
-        await asyncio.sleep(0.75)
-        player+=1
+        is_sucessfully_read, frame = cap.read()
+        
+        cv2.flip(frame,1)
+                
+        hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+        hue,sat,val = cv2.split(hsv)
+        
+        # Apply thresholding
+        hthresh = cv2.inRange(np.array(hue),np.array(hmn),np.array(hmx))
+        sthresh = cv2.inRange(np.array(sat),np.array(smn),np.array(smx))
+        vthresh = cv2.inRange(np.array(val),np.array(vmn),np.array(vmx))
+
+        # AND h s and v
+        tracking = cv2.bitwise_and(hthresh,cv2.bitwise_and(sthresh,vthresh))
+
+        # Some morpholigical filtering
+        dilation = cv2.dilate(tracking,kernel,iterations = 1)
+        closing = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
+        closing = cv2.GaussianBlur(closing,(5,5),0)
+
+        # Detect circles using HoughCircles 100/50
+        circles = cv2.HoughCircles(closing,cv2.HOUGH_GRADIENT,2,120,param1=70,param2=40,minRadius=10,maxRadius=20)
+
+        x = 0
+        y = 0
+        
+        #Draw Circles
+        if circles is not None:
+                for i in circles[0,:]:
+                    x  = int(round(i[0]))
+                    y  = int(round(i[1]))
+                    cv2.circle(frame,(int(round(i[0])),int(round(i[1]))),int(round(i[2])),(0,255,0),1)
+                    cv2.circle(frame,(int(round(i[0])),int(round(i[1]))),2,(0,255,0),5)
+        
+        if(x>0 and y>0):
+            print('x: '+str(x))
+            print('y: '+str(y))
+            await websocket.send(str(x)+';'+str(y))
+            
+        await asyncio.sleep(0.25)
 				
-start_server = websockets.serve(time, '127.0.0.1', 5678)
+start_server = websockets.serve(time, '10.0.1.44', 5678)
 print('websocket started...')
 
 asyncio.get_event_loop().run_until_complete(start_server)
